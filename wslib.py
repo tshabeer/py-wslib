7ee0baab2b7a1dc8debbc303c3ffe2b06d847e32
Roderick Baier <mail@roderick.de>
2012-03-26 00:19:19 +0200

    opcodes; shared BaseWebSocket

--- wslib.py
+++ wslib.py
@@ -38,25 +38,46 @@ STATE_CLOSED = 3
 
 GUID = "258EAFA5-E914-47DA-95CA-C5AB0DC85B11"
 
+OPCODE_TEXT = 0x1
+OPCODE_BINARY = 0x2
+OPCODE_CLOSE = 0x8
+OPCODE_PING = 0x9
+OPCODE_PONG = 0xA
+OPCODES = [OPCODE_TEXT,
+           OPCODE_BINARY,
+           OPCODE_CLOSE,
+           OPCODE_PING,
+           OPCODE_PONG]
+
+
+class WebSocketError(Exception): pass
+
 
 class BaseWebSocket(object):
     
     _ready_state = STATE_CONNECTING
+    _frame_reader = None
+    
+    def __init__(self, handler, mask):
+        self.handler = handler
+        self.mask = mask
     
     def _send_raw(self, data):
         self.socket.sendall(str(data))
     
     def _send_close_handshake(self):
         if self._ready_state == STATE_OPEN:
-            pass # TODO
+            self._ready_state = STATE_CLOSING
+            # TODO
         else:
             print "error send_close_handshake"
     
     def close(self):
-        self._ready_state = STATE_CLOSING
-        self._send_close_handshake()
-        self.frame_reader.stop()
-        self.socket.close()
+        if self._ready_state > STATE_CONNECTING:
+            self._ready_state = STATE_CLOSING
+            self._send_close_handshake()
+            self._frame_reader.stop()
+            self.socket.close()
         self._ready_state = STATE_CLOSED
 
 
@@ -65,11 +86,10 @@ class WebSocket(BaseWebSocket):
     header_fields = {}
     
     def __init__(self, url, handler, protocol=None, mask=True):
+        BaseWebSocket.__init__(self, handler, mask)
         self.handshake = ClientHandshake(url)
         print "handshake:", self.handshake
         self.url = url
-        self.handler = handler
-        self.mask = mask
     
     def add_header_field(self, key, value):
         self.header_fields[key.strip()] = value.strip()
@@ -81,8 +101,8 @@ class WebSocket(BaseWebSocket):
         print headers
         if headers['Sec-WebSocket-Accept'] != self.handshake.key_accept(self.handshake.nonce):
             raise Exception("accept key error")
-        self.frame_reader = FrameReader(self.socket, self.handler.onmessage)
-        self.frame_reader.start()
+        self._frame_reader = FrameReader(self.socket, self.handler.onmessage)
+        self._frame_reader.start()
         self._ready_state = STATE_OPEN
         self.handler.onopen(None)
     
@@ -103,7 +123,7 @@ class WebSocket(BaseWebSocket):
             print "headers:", headers
             return headers
         else:
-            raise Exception("handshake failed")
+            raise WebSocketError("handshake failed")
     
     def send(self, message):
         print "sending:", message
@@ -132,14 +152,15 @@ class WebSocket(BaseWebSocket):
 
 class ServerWebSocket(BaseWebSocket):
     
-    def __init__(self, socket, onmessage):
+    def __init__(self, socket, handler, mask=True):
+        BaseWebSocket.__init__(self, handler, mask)
         print "creating ServerWebSocket instance"
         self.socket = socket
         request, headers = self._read_handshake()
         handshake = ServerHandshake(client_key=headers['Sec-WebSocket-Key'])
         self._send_raw(handshake)
-        frame_reader = FrameReader(self.socket, onmessage)
-        frame_reader.start()
+        self._frame_reader = FrameReader(self.socket, handler.onmessage)
+        self._frame_reader.start()
     
     def _read_handshake(self):
         handshake = self.socket.recv(4096)
@@ -154,78 +175,11 @@ class ServerWebSocket(BaseWebSocket):
             headers[line[0]] = line[1]
         print "headers:", headers
         if not "Upgrade" in headers and headers['Upgrade'] is not "websocket":
-            raise Exception("not upgrade")
+            raise WebSocketError("not upgrade")
         return request[1], headers
     
     def send(self, data):
         self._send_raw(data)
-
-
-class Frame(object):
-    
-    def __init__(self, opcode, data, masking):
-        self.opcode = opcode
-        self.data = data
-        self.masking = masking
-    
-    def __repr__(self):
-        fin = 0x80
-        length = len(self.data)
-        frame = struct.pack("!B", fin | self.opcode)
-        if length < 126:
-            if self.masking:
-                length = 0x80 | length
-            frame += struct.pack("!B", length)
-        elif length <= 65535:
-            len_field = 126
-            if self.masking:
-                len_field = 0x80 | len_field
-            frame += struct.pack("!BH", len_field, length)
-        else:
-            len_field = 127
-            if self.masking:
-                len_field = 0x80 | len_field
-            frame += struct.pack("!BQ", len_field, length)
-        if self.masking:
-            masking_key = array.array("B", os.urandom(4))
-            frame += masking_key.tostring()
-            data = array.array("B", self.data)
-            for i in range(len(data)):
-                data[i] = data[i] ^ masking_key[i % 4]
-            frame += data.tostring()
-        else:
-            frame += self.data
-        return frame
-
-
-class TextFrame(Frame):
-    
-    def __init__(self, message, masking=True):
-        Frame.__init__(self, 0x1, message, masking)
-
-
-class BinaryFrame(Frame):
-    
-    def __init__(self, data, masking=True):
-        Frame.__init__(self, 0x2, data, masking)
-
-
-class CloseFrame(Frame):
-    
-    def __init__(self):
-        Frame.__init__(self, 0x8, '', False)
-
-
-class PingFrame(Frame):
-    
-    def __init__(self):
-        Frame.__init__(self, 0x9,'' , False)
-
-
-class PongFrame(Frame):
-    
-    def __init__(self):
-        Frame.__init__(self, 0xA, '', False)
 
 
 class Handshake(object):
@@ -299,6 +253,73 @@ class ServerHandshake(Handshake):
         return None
 
 
+class Frame(object):
+    
+    def __init__(self, opcode, data, masking):
+        self.opcode = opcode
+        self.data = data
+        self.masking = masking
+    
+    def __repr__(self):
+        fin = 0x80
+        length = len(self.data)
+        frame = struct.pack("!B", fin | self.opcode)
+        if length < 126:
+            if self.masking:
+                length = 0x80 | length
+            frame += struct.pack("!B", length)
+        elif length <= 65535:
+            len_field = 126
+            if self.masking:
+                len_field = 0x80 | len_field
+            frame += struct.pack("!BH", len_field, length)
+        else:
+            len_field = 127
+            if self.masking:
+                len_field = 0x80 | len_field
+            frame += struct.pack("!BQ", len_field, length)
+        if self.masking:
+            masking_key = array.array("B", os.urandom(4))
+            frame += masking_key.tostring()
+            data = array.array("B", self.data)
+            for i in range(len(data)):
+                data[i] = data[i] ^ masking_key[i % 4]
+            frame += data.tostring()
+        else:
+            frame += self.data
+        return frame
+
+
+class TextFrame(Frame):
+    
+    def __init__(self, message, masking=True):
+        Frame.__init__(self, OPCODE_TEXT, message, masking)
+
+
+class BinaryFrame(Frame):
+    
+    def __init__(self, data, masking=True):
+        Frame.__init__(self, OPCODE_BINARY, data, masking)
+
+
+class CloseFrame(Frame):
+    
+    def __init__(self):
+        Frame.__init__(self, OPCODE_CLOSE, '', False)
+
+
+class PingFrame(Frame):
+    
+    def __init__(self):
+        Frame.__init__(self, OPCODE_PING, '', False)
+
+
+class PongFrame(Frame):
+    
+    def __init__(self):
+        Frame.__init__(self, OPCODE_PONG, '', False)
+
+
 class FrameReader(Thread):
     
     def __init__(self, socket, onmessage):
@@ -323,7 +344,8 @@ class FrameReader(Thread):
                     break
                 header, length = struct.unpack("!BB", data)
                 opcode = header & 0xf
-                # TODO opcode ok? if not -> disconnect
+                if not opcode in OPCODES:
+                    raise WebSocketError("unknown or unsupported opcode")
                 reserved = header & 0x70
                 masked = length & 0x80
                 length = length & 0x7f
@@ -353,24 +375,13 @@ class FrameReader(Thread):
         print "FrameReader: stopping"
 
 
-class WebSocketHandler(object):
-    
-    def onopen(self, protocol):
-        pass
-    
-    def onmessage(self, message):
-        pass
-    
-    def onclose(self):
-        pass
-
-
 class WebSocketServer(object):
     
-    def __init__(self, host, port, handler):
+    def __init__(self, host, port, handler, origins=[]):
         self.host = host
         self.port = port
         self.handler = handler
+        self.origins = origins
     
     def onconnect(self, client):
         sub_protocol = self.choose_sub_protocol(client.sub_protocols)
@@ -387,7 +398,19 @@ class WebSocketServer(object):
         server_socket.listen(128)
         while True:
             client_socket, client_address = server_socket.accept()
-            ServerWebSocket(client_socket, self.handler.onmessage)
+            ServerWebSocket(client_socket, self.handler)
     
     def choose_sub_protocol(sub_protocols=None):
         return None
+
+
+class WebSocketHandler(object):
+    
+    def onopen(self, protocol):
+        pass
+    
+    def onmessage(self, message):
+        pass
+    
+    def onclose(self):
+        pass
